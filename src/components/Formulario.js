bc39821deb0f716d9d7035747cdb11d04703f237
KevinTortoledoRodriguez <60956746+KevinTortoledoRodriguez@users.noreply.github.com>
2021-08-08 17:56:05 -0600

    ultimos cambios

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 const Formulario = () => {
+    const [busqueda, setBusqueda] = useState({
+        ciudad: '',
+        pais: '',
+    })
+
+    const [error, setError] = useState(false)
+
+    //extraer la ciudad y el pais
+    const {ciudad, pais} = busqueda;
+
+
+
+    //funcion que coloca los elementos en el state
+    const handleChange = (e) => {
+        //actualizar el state
+        setBusqueda({...busqueda, [e.target.name]:e.target.value})
+    }
+    
+    //cuando el usuario da submit al form
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //validar
+
+        //pasarlo al componente principal
+    }
+
     return ( 
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <div className="input-field col s12">
-                <input type="text" name="ciudad" id="ciudad" />
+                <input type="text" name="ciudad" id="ciudad" value={ciudad} onChange={handleChange}/>
                 <label htmlFor="ciudad">Ciudad:</label>
             </div>
             <div className="input-field col s12">
-                <select name="pais" id="pais">
+                <select name="pais" id="pais" value={pais} onChange={handleChange}>
                     <option value="">-- Seleccione un pais</option>
                 </select>
                 <label htmlFor="pais">País:</label>
